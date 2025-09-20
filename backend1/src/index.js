34ca0dd40218969e820cd5ec6eb71ae122cc095b
rksorry7255tu <rk7255053708@gmail.com>
2025-09-20 09:40:06 +0530

    commited

--- backend1/src/index.js
+++ backend1/src/index.js
@@ -1,7 +1,22 @@
 import dotenv from "dotenv";
-import connectDB from "./db/db.js";
 dotenv.config({
   path: ".env",
 });
+import connectDB from "./db/db.js";
+import app from "./app.js";
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(
+        `Server is running successfully on port: ${process.env.PORT}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDb connection failed", err);
+  });
